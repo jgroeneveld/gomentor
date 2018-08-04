e73589eab2e608a70356891b850b9e6fb887a182
Jaap Groeneveld <jgroeneveld@fastmail.com>
2018-08-04 11:19:35 +0200

    add some fun html

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"github.com/labstack/echo/middleware"
 	"os"
 	"log"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
 )
 
 type Manager struct {
 	ID        int    `json:"id"`
 	FirstName string `json:"first_name"`
-	LastName string `json:"last_name"`
+	LastName  string `json:"last_name"`
 }
 
 type PagedCollectionResponse struct {
@@ -26,6 +29,16 @@ type PageInfo struct {
 	PageSize      int  `json:"page_size"`
 }
 
+type QuoteApiResponse struct {
+	QuoteText   string `json:"quoteText"`
+	QuoteAuthor string `json:"quoteAuthor"`
+}
+
+type Quote struct {
+	Text   string `json:"text"`
+	Author string `json:"author"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -37,7 +50,13 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	managers := e.Group("/managers")
+	e.Static("/", "static")
+
+	api := e.Group("/api")
+
+	api.GET("/random_quote", randomQuote)
+
+	managers := api.Group("/managers")
 	managers.GET("", listManagers)
 	managers.GET("/:id", getManager)
 
@@ -53,6 +72,49 @@ func listManagers(ctx echo.Context) error {
 
 func getManager(ctx echo.Context) error {
 	return ctx.JSON(200, &Manager{ID: 12, FirstName: "Jon", LastName: "Snow"})
+}
+
+func randomQuote(ctx echo.Context) error {
+	url := "https://api.forismatic.com/api/1.0/?method=getQuote&key=457635&format=json&lang=en"
+
+	quoteResponse := QuoteApiResponse{}
+	err := getJSON(url, &quoteResponse)
+	if err != nil {
+		return err
+	}
+
+	quote := Quote{
+		Author: quoteResponse.QuoteAuthor,
+		Text:   quoteResponse.QuoteText,
+	}
+
+	return ctx.JSON(200, quote)
+}
+
+func getJSON(url string, target interface{}) error {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, target)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func fakePageInfo() PageInfo {
---
+++ static/main.js
@@ -0,0 +1,59 @@
+function init() {
+    requestManagers(displayManagers);
+
+    requestManager(12, displayManager);
+
+    loadQuote();
+}
+
+function requestManagers(callback) {
+    jsonRequest("/api/managers", callback);
+}
+
+function displayManagers(managersResponse) {
+    document.getElementById("listManagers").innerHTML = JSON.stringify(managersResponse, null, 2);
+}
+
+function requestManager(id, callback) {
+    jsonRequest("/api/managers/" + id, callback);
+}
+
+function displayManager(managerResponse) {
+    document.getElementById("getManager").innerHTML = JSON.stringify(managerResponse, null, 2);
+}
+
+function loadQuote() {
+    const button = document.getElementById("quoteButton");
+    button.disabled = true;
+
+    requestQuote(function (quote) {
+        displayQuote(quote);
+        button.disabled = false;
+    });
+}
+
+function requestQuote(callback) {
+    var response = jsonRequest("/api/random_quote", callback);
+}
+
+function displayQuote(quote) {
+    document.getElementById("quote").style = "visibility: visible";
+    let quoteElement = document.getElementById("quote");
+
+    quoteElement.setAttribute("cite", quote.author);
+    quoteElement.innerHTML = quote.text;
+}
+
+function jsonRequest(url, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            callback(JSON.parse(xhttp.responseText));
+        }
+    };
+
+    xhttp.send();
+}
